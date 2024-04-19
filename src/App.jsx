f60926f22bfa67dd262668c73a160ada1e4e40aa
Alisson F André <alissonfeliciano0@gmail.com>
2024-04-18 21:47:52 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Menu_Superior from './components/MenuSuperior';
 import Manutencao_Tarefas from './components/manutencao_tarefas';
 import FormularioLogin from './components/login';
 import Cadastrar_Usuarios from './components/cadastrar_usuario';
+import Agendar_Servicos from "./components/agendar_servicos";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider, useAuth } from './components/AuthProvider';
 import './css/index.css';
 import './css/cadastro.css';
 import './css/prestador.css';
+import './css/agendar.css';
 
 
 
@@ -27,6 +29,7 @@ const RoutesWithAuth = () => {
                 <Route path="/login" element={<FormularioLogin />} />
                 <Route path="/" element={autenticado ? <Navigate to="/tarefas" /> : <FormularioLogin />} />
                 <Route path="/prestador" element={<Cadastrar_Prestador />} />
+                <Route path="/agendar" element={<Agendar_Servicos />} />
                 <Route path="/manutencao" element={<ProtectedRoute><Manutencao_Tarefas /></ProtectedRoute>} />
                 <Route path="/user" element={<Cadastrar_Usuarios />} />
             </Routes>
